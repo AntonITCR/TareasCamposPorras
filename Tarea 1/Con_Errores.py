b112e07c0982b38166db7b60393f42d56c8f8e26
AntonITCR <99512574+AntonITCR@users.noreply.github.com>
2022-02-17 21:51:02 -0600

    Codigos Flake 8
    Aquí se encuentra el código con errores identificables y otro con los errores corregidos

---
+++ Tarea 1/Con_Errores.py
@@ -0,0 +1,10 @@
+#Comentario incocorrecto
+ 
+# Error: espacio en linea anterior
+def func(x):
+    # Error: espacio antes de "("
+    print (x)
+    return x*2
+
+
+func(5)
---
+++ Tarea 1/Sin_Errores.py
@@ -0,0 +1,8 @@
+# Comentario correcto
+
+def func(x):
+    print(x)
+    return x*2
+
+
+func(5)
